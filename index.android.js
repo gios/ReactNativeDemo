c00d3f27b64a16d6817aefbbec743a4763e17b50
Pavlo Blazhchuk <pblazhchuk@gmail.com>
2016-02-27 15:20:34 +0200

    eslint integration

--- index.android.js
+++ index.android.js
@@ -2,30 +2,34 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
+'use strict'
 import React, {
   AppRegistry,
   Component,
+  Image,
   StyleSheet,
   Text,
   View
-} from 'react-native';
+} from 'react-native'
+
+var MOCKED_MOVIES_DATA = [
+  {
+    title: 'Title',
+    year: '2015',
+    posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}
+  }
+]
 
 class shophub extends Component {
   render() {
+    var movie = MOCKED_MOVIES_DATA[0]
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source={{uri: movie.posters.thumbnail}} />
       </View>
-    );
+    )
   }
 }
 
@@ -34,18 +38,18 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF'
   },
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    margin: 10,
+    margin: 10
   },
   instructions: {
     textAlign: 'center',
     color: '#333333',
-    marginBottom: 5,
-  },
-});
+    marginBottom: 5
+  }
+})
 
-AppRegistry.registerComponent('shophub', () => shophub);
+AppRegistry.registerComponent('shophub', () => shophub)
